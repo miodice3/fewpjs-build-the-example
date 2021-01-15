c6c5678c92be00b4638c1df1e5afbdcc82beb4b7
Michael Iodice <miodice3@gmail.com>
2021-01-14 17:04:34 -0700

    commit tree fa933c6f303e698d837fff404457559276a85b75

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
 
+let error = document.getElementById('modal')
 
+document.addEventListener("DOMContentLoaded", removeError)
+document.addEventListener("click", likedMessage)
 
+function removeError(e){
+  error.hidden = true
+}
+
+function changeError(argument){
+  error.hidden = argument
+}
+
+function likedMessage(e){
+  if (e.target.innerText === EMPTY_HEART){
+    mimicServerCall()
+      .then(function(response){
+        e.target.innerText = FULL_HEART
+        return response
+      })
+      .catch(function(error){
+        changeError(false)
+        setTimeout(() => changeError(true), 5000)
+      })
+  } else if (e.target.innerText === FULL_HEART) {
+    mimicServerCall()
+      .then(function(response){
+        e.target.innerText = EMPTY_HEART
+        return response
+      })
+      .catch(function(error){
+        changeError(false)
+        setTimeout(() => changeError(true), 5000)
+      })
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
